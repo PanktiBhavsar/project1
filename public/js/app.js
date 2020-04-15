eb7fe05e72e8f29367cfadb7f44bc1aea8192e03
panktibh77 <panktibhavsar1@gmail.com>
2020-04-15 12:19:22 +0530

    hello

---
+++ public/js/app.js
@@ -0,0 +1,32 @@
+console.log('Client side javascript file loaded!')
+
+const weatherform = document.querySelector('form')
+const search = document.querySelector('input')
+const messageone = document.querySelector('#message-1')
+const messageTwo = document.querySelector('#message-2')
+
+
+//messageone.textContent = 'from javascript'
+
+weatherform.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const location = search.value
+
+    messageone.textContent = 'Loading'
+    messageTwo.textContent = ''
+
+    fetch('http://localhost:3000/weather?address=' + location).then((response) => {
+    response.json().then((data) => {
+        if(data.error){
+            messageone.textContent = data.error
+        }else{
+            messageone.textContent = data.location
+            messageTwo.textContent = data.forecast
+            console.log(data.location)
+            console.log(data.forecast)
+        }
+    })
+    })
+    //console.log(location)
+})
