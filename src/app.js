9bc5dda86b47a1b892a4892bc3ddd633cc1aa783
panktibh77 <panktibhavsar1@gmail.com>
2020-04-15 12:44:35 +0530

    yoo

--- src/app.js
+++ src/app.js
@@ -19,7 +19,7 @@ app.use(express.static(publicDirectoryPath))
 app.get('', (req, res) => {
     res.render('index',{
         title: 'Weather App',
-        name: 'Pankti Bhavsar'
+        name: 'Pankti'
     })
 })
 
